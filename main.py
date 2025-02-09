7d4ef0690bc852fde9534da123be68002f26c07a
TarunGanesh <tarun.21bce8974@vitapstudent.ac.in>
2025-02-09 17:54:41 +0530

    update main.py to connect to db

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from flask import Flask
 from app.routes import setup_routes
+from app.models import db
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+# Initialize the database
+db.init_app(app)
+
+# Create database tables
+with app.app_context():
+    db.create_all()
 
 # Initialize routes
 setup_routes(app)
